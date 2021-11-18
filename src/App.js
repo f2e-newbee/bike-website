290283b6ee1abe667b1133c2ec8e08b773e7fcfe
Joanne Lin <joanne@miniasp.com>
2021-11-19 01:23:24 +0800

    feat: 共用背景圖 & 移除未使用到的程式碼

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, RentBikeQuery, RouteQuery} from "./pages";
-import { ErrorPage } from "./pages/error-page/ErrorPage";
 import { Provider } from "react-redux";
-import store from "./core/store";
+import { store } from "./store/Store";
+import BackGround from "./components/backGround";
+import { ErrorPage } from "./pages/error-page/ErrorPage";
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Routes basename="bike-website">
-          <Route exact path="/">
-            <Route path="/" element={<Home />} />
-            <Route path="/rent-bike-query" element={<RentBikeQuery />} />
-            <Route path="/route-query" element={<RouteQuery />} />
-            <Route path="*" element={<ErrorPage />}/>
-          </Route>
+        <Routes>
+          <Route path="/" element={<BackGround />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- src/components/backGround/index.js
+++ src/components/backGround/index.js
@@ -5,11 +5,13 @@ import { ReactComponent as Cell3Icon } from "../../assets/img/cell-3.svg";
 import { ReactComponent as Cell4Icon } from "../../assets/img/cell-4.svg";
 import { ReactComponent as Cell5Icon } from "../../assets/img/cell-5.svg";
 import { ReactComponent as Cell6Icon } from "../../assets/img/cell-6.svg";
+import { Home, RentBikeQuery, RouteQuery } from "../../pages";
+import { Routes, Route } from "react-router-dom";
 
 /**
  * 背景軟泥圖
  */
-const BackGround = ({ children }) => {
+const BackGround = () => {
   return (
     <div className="h-full bg-beige overflow-hidden relative md:overflow-hidden">
       <Cell1Icon className="absolute right-0 transform md:translate-x-1/3 md:-translate-y-1/3 translate-x-1/2 -translate-y-1/2" />
@@ -18,7 +20,11 @@ const BackGround = ({ children }) => {
       <Cell4Icon className="absolute left-0 transform translate-x-36 translate-y-16 hidden md:block" />
       <Cell5Icon className="absolute left-0 transform -translate-x-1/3 translate-y-32" />
       <Cell6Icon className="absolute left-0 bottom-0 transform -translate-x-10 translate-y-20" />
-      {children}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rent-bike-query" element={<RentBikeQuery />} />
+        <Route path="/route-query" element={<RouteQuery />} />
+      </Routes>
     </div>
   );
 };
