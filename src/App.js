06d1fa45d03b2d1054ac4469778abc54efe9a0b7
Joanne Lin <joanne@miniasp.com>
2021-11-19 01:51:02 +0800

    fix: 調整路由設定

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/Store";
-import BackGround from "./components/backGround";
-import { ErrorPage } from "./pages/error-page/ErrorPage";
+import Root from "./components/root";
+import { Home, RentBikeQuery, RouteQuery, ErrorPage } from "./pages";
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<BackGround />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<Root />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/rent-bike-query" element={<RentBikeQuery />} />
+            <Route path="/route-query" element={<RouteQuery />} />
+          </Route>
+          <Route path="*"  element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- src/pages/rent-bike-query/index.js
+++ src/pages/rent-bike-query/index.js
@@ -4,4 +4,4 @@ export const RentBikeQuery = () => {
   return <div>單車租借查詢</div>;
 };
 
-export default RentBikeQuery;
+
