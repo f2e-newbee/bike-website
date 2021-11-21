358e2e8fb5e4bc6778e38c30fc0e7f031ef1f4f9
Joanne Lin <joanne@miniasp.com>
2021-11-21 23:23:51 +0800

    feat: map & 安裝google-map-react相關套件

---
+++ src/pages/rent-bike-query/Map.js
@@ -0,0 +1,112 @@
+import React, { useRef, useState } from "react";
+import GoogleMapReact from "google-map-react";
+import { API_KEY } from "../../BikeKey";
+import useSupercluster from "use-supercluster";
+import { ReactComponent as BikeIcon } from "../../assets/img/bike.svg";
+
+const Map = ({ center, list }) => {
+  const mapRef = useRef();
+  // get map bounds
+  const [bounds, setBounds] = useState(null);
+  const [zoom, setZoom] = useState(10);
+
+  const points = list.map((item) => ({
+    type: "Feature",
+    properties: {
+      cluster: false,
+      id: item.StationUID,
+      name: item.StationName.Zh_tw,
+      address: item.StationAddress.Zh_tw,
+      showDetail: false,
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        item.StationPosition.PositionLon,
+        item.StationPosition.PositionLat,
+      ],
+    },
+  }));
+  const Marker = ({ children }) => children;
+
+  let { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom,
+    options: { radius: 75, maxZoom: 20 },
+  });
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: API_KEY }}
+        defaultCenter={{ lat: center.latitude, lng: center.longitude }}
+        defaultZoom={15}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map }) => {
+          mapRef.current = map;
+        }}
+        onChange={({ zoom, bounds }) => {
+          setZoom(zoom);
+          setBounds([
+            bounds.nw.lng,
+            bounds.se.lat,
+            bounds.se.lng,
+            bounds.nw.lat,
+          ]);
+        }}
+      >
+        {/* markers here */}
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const { cluster: isCluster, point_count: pointCount } =
+            cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={`cluster-${cluster.id}`}
+                lat={latitude}
+                lng={longitude}
+              >
+                <div
+                  className="text-white rounded-full bg-blue-600 flex items-center justify-center p-3"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 20}px`,
+                    height: `${10 + (pointCount / points.length) * 20}px`,
+                  }}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      20
+                    );
+                    mapRef.current.setZoom(expansionZoom);
+                    mapRef.current.panTo({ lat: latitude, lng: longitude });
+                  }}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+
+          return (
+            <Marker key={cluster.properties.id} lat={latitude} lng={longitude}>
+              <div>{cluster.properties.showDetail}</div>
+              <button
+                className="bg-text-dark rounded-full p-1.5 opacity-80 w-8 h-8"
+                onClick={() =>
+                  alert(cluster.properties.name, cluster.properties.address)
+                }
+              >
+                <BikeIcon className="w-full h-full text-white" />
+              </button>
+            </Marker>
+          );
+        })}
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
--- src/pages/route-query/index.js
+++ src/pages/route-query/index.js
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { fetchApi } from "../../service/Service";
 import { ReactComponent as RouteIcon } from "../../assets/img/route.svg";
 import { ReactComponent as Logo } from "../../assets/img/logo.svg";
-import { Paper , Stack } from "@material-ui/core";
+import { Paper, Stack } from "@material-ui/core";
 import Toggle from "../../components/toggle";
 import SearchBar from "../../components/searchBar/SearchBar";
 import CustomTabs from "../../components/Tabs";
 import SelectCity from "../../components/SelectCity";
 
-
-
 export const RouteQuery = () => {
   /** 偵測螢幕寬度 */
   let screenWidth = Number(document.body.clientWidth);
-  window.addEventListener("resize", function(event) {});
+  window.addEventListener("resize", function (event) {});
 
   const [routeData, setRouteData] = useState(null);
   const [keyWord, setKeyWord] = useState("");
@@ -23,22 +21,19 @@ export const RouteQuery = () => {
   // 依照關鍵字搜尋的list
   const [filterList, setFilterList] = useState([]);
 
+  const getCity = useCallback(() => {
+    if (city) {
+      fetchApi(`/v2/Cycling/Shape/${city}?format=JSON`).then((response) => {
+        if (response && response.status === 200) {
+          setRouteData(response.data);
+        }
+      });
+      setKeyWord("");
+    }
+  }, [city]);
   useEffect(() => {
     getCity();
-  }, [city]);
-
-  function getCity() {
-    if(city){
-      fetchApi(`/v2/Cycling/Shape/${city}?format=JSON`).then(
-        (response) => {
-          if (response && response.status === 200) {
-            setRouteData(response.data);
-          }
-        }
-      );
-      setKeyWord('');
-    }
-  }
+  }, [city, getCity]);
 
   useEffect(() => {
     if (routeData) {
@@ -53,7 +48,6 @@ export const RouteQuery = () => {
     }
   }, [keyWord, list]);
 
-
   function handleSearch() {
     if (keyWord && keyWord.trim() && city) {
       const filter = list.filter((item) => {
@@ -65,79 +59,97 @@ export const RouteQuery = () => {
 
   return (
     <>
-    <div className="z-10 relative px-4">
-    <Logo className="absolute w-36 md:hidden block top-0"/>
-      <div className="w-full h-screen relative z-10">
-        <section className="
+      <div className="z-10 relative px-4">
+        <Logo className="absolute w-36 md:hidden block top-0" />
+        <div className="w-full h-screen relative z-10">
+          <section
+            className="
         mx-auto w-full max-w-screen-xl md:min-h-notebook h-5/6	sm:bg-white sm:shadow-xl border-gray-200	sm:border
-        top-2/4 left-2/4  absolute transform -translate-y-2/4 -translate-x-2/4 rounded-3xl md:pb-8 pb-0">
-          <div className="md:h-1/5 h-auto pt-10 relative lg:px-20 md:px-12 sm:px-10 px-6 sm:block hidden">
-            <Logo className="absolute w-36 md:block hidden"/>
-            <Toggle />
-          </div>
-          <div className="md:h-4/5 sm:h-5/6 h-full w-full grid  md:grid-rows-9 md:grid-cols-2  grid-cols-1 gap-4 
-              lg:px-20 md:px-12 sm:px-10 px-0 md:py-0 sm:py-6 py-4 pt-0">
-            <div className="bg-tertiary-dark	row-span-3 rounded-3xl md:text-base text-sm	lg:p-8 p-4 lg:py-4">
-            <Stack className="text-white md:text-lg text-base mb-2 font-bold" direction="row" spacing={1} justifyContent="center">
-              <RouteIcon className="w-6 h-6" />
-              <span>路線查詢</span>
-            </Stack>
-              <div className="flex mb-4">
-                {/* <button className="bg-purple-custom sm:px-6 px-2 sm:py-2 py-1 rounded-3xl text-white font-bold  sm:w-28 w-24">離我最近</button> */}
+        top-2/4 left-2/4  absolute transform -translate-y-2/4 -translate-x-2/4 rounded-3xl md:pb-8 pb-0"
+          >
+            <div className="md:h-1/5 h-auto pt-10 relative lg:px-20 md:px-12 sm:px-10 px-6 sm:block hidden">
+              <Logo className="absolute w-36 md:block hidden" />
+              <Toggle />
+            </div>
+            <div
+              className="md:h-4/5 sm:h-5/6 h-full w-full grid  md:grid-rows-9 md:grid-cols-2  grid-cols-1 gap-4 
+              lg:px-20 md:px-12 sm:px-10 px-0 md:py-0 sm:py-6 py-4 pt-0"
+            >
+              <div className="bg-tertiary-dark	row-span-3 rounded-3xl md:text-base text-sm	lg:p-8 p-4 lg:py-4">
+                <Stack
+                  className="text-white md:text-lg text-base mb-2 font-bold"
+                  direction="row"
+                  spacing={1}
+                  justifyContent="center"
+                >
+                  <RouteIcon className="w-6 h-6" />
+                  <span>路線查詢</span>
+                </Stack>
+                <div className="flex mb-4">
+                  {/* <button className="bg-purple-custom sm:px-6 px-2 sm:py-2 py-1 rounded-3xl text-white font-bold  sm:w-28 w-24">離我最近</button> */}
                   <div className="sm:w-32 w-28 flex">
                     <SelectCity city={city} setCity={setCity} />
                   </div>
-                  {
-                    city ? null :   <p className="text-red-500 ml-4">**請選擇縣市**</p>
-                  }
-              </div>
-              <SearchBar
+                  {city ? null : (
+                    <p className="text-red-500 ml-4">**請選擇縣市**</p>
+                  )}
+                </div>
+                <SearchBar
                   setKeyWord={setKeyWord}
                   keyWord={keyWord}
                   handleSearch={handleSearch}
                 />
-            </div>
-            <div className="bg-gray-100  row-span-9  rounded-3xl hidden md:block">
-            </div>
-            <div className="bg-red-50 row-span-6  rounded-3xl p-5 h-full overflow-y-scroll sm:grid hidden">
-              <p>共 <span className="font-bold text-primary">{filterList.length} </span>筆資料</p>
-                {
-                  filterList &&  filterList.length > 0 ?
-                  filterList.map((item,index) =>{
-                    return <RouteDataCard data={item} key={`${item.RouteName}${index}`} />
-                  })
-                  : "查無資料QAQ"
-                }
-            </div>
-            {
-              screenWidth < 480 ?  <CustomTabs data={filterList}/> : null
-            }
+              </div>
+              <div className="bg-gray-100  row-span-9  rounded-3xl hidden md:block"></div>
+              <div className="bg-red-50 row-span-6  rounded-3xl p-5 h-full overflow-y-scroll sm:grid hidden">
+                <p>
+                  共{" "}
+                  <span className="font-bold text-primary">
+                    {filterList.length}{" "}
+                  </span>
+                  筆資料
+                </p>
+                {filterList && filterList.length > 0
+                  ? filterList.map((item, index) => {
+                      return (
+                        <RouteDataCard
+                          data={item}
+                          key={`${item.RouteName}${index}`}
+                        />
+                      );
+                    })
+                  : "查無資料QAQ"}
+              </div>
+              {screenWidth < 480 ? <CustomTabs data={filterList} /> : null}
             </div>
           </section>
+        </div>
       </div>
-    </div>
-    <div className="w-full h-screen opacity-50 top-0 left-0 bg-white absolute"></div>
-  </>
+      <div className="w-full h-screen opacity-50 top-0 left-0 bg-white absolute"></div>
+    </>
   );
 };
 
-
-const RouteDataCard  = ({data}) =>{
-  return(
-    <Paper className="w-full min-h-24 my-2 p-4" elevation={2} >
-      <div className="flex justify-between	h-full"> 
+const RouteDataCard = ({ data }) => {
+  return (
+    <Paper className="w-full min-h-24 my-2 p-4" elevation={2}>
+      <div className="flex justify-between	h-full">
         <div>
           <h3 className="font-bold text-gray-500">{data.RouteName}</h3>
           <p className="text-gray-400 text-sm">起點: {data.RoadSectionStart}</p>
           <p className="text-gray-400 text-sm	">終點: {data.RoadSectionEnd}</p>
-          {
-            data.CyclingLength > 1000 ? 
-            <p className="text-primary text-sm	">全長: {Math.round(data.CyclingLength / 100, -1)} 公里</p>
-            :
-            <p className="text-green text-sm	">全長: <span className="text-green-400">{data.CyclingLength}</span>公尺</p>
-          }
+          {data.CyclingLength > 1000 ? (
+            <p className="text-primary text-sm	">
+              全長: {Math.round(data.CyclingLength / 100, -1)} 公里
+            </p>
+          ) : (
+            <p className="text-green text-sm	">
+              全長: <span className="text-green-400">{data.CyclingLength}</span>
+              公尺
+            </p>
+          )}
         </div>
       </div>
     </Paper>
-  )
-}
+  );
+};
